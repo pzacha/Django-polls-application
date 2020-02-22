7ab14efe2cf396634b5d2483896495431fe819fc
Piotr Zacha <pzacha11@gmail.com>
2020-02-22 15:09:36 +0100

    Model update and migrations

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Question(models.Model):
-    question_text = models.CharField(max_lengh=200)
+    question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
 
