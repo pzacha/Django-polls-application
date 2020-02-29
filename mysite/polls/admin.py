0d641d75bf59905acfef7802e4222d2092e75ae4
Piotr Zacha <pzacha11@gmail.com>
2020-02-29 09:51:31 +0100

    Changed polls admin options

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 
 from .models import Question
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+    fields = ["pub_date", "question_text"]
+
+
+admin.site.register(Question, QuestionAdmin)
